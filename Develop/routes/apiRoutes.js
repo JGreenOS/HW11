7e900b853df006a5113456b0fcfe2bf996284841
JGreenOS <jsciteach@gmail.com>
2021-03-20 16:36:00 -0400

    routing information

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,21 +1,27 @@
 //create const variables for data source arrays
 const router = require('express').Router();
-const fs = require('fs');
+const store = require("../db/store")
 
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+        .then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+
+    store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
 
 ///ROUTING
 
-module.exports = (app) => {
-//API get requests here
 
 
-router.get("/notes", function(req, res) {
 
-    fs.readFile("./db/db.json", "utf8", function(err, data){
-//error code
-if (err) throw err;
-res.json(JSON.parse(data));
-    });
-});
-
-}
+module.exports = router;
