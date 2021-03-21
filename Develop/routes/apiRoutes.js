bcffe87a9e9474e7980c5747f5e000f2c55b2df3
JGreenOS <jsciteach@gmail.com>
2021-03-21 12:58:53 -0400

    updated routing and added uuid for unique id, fixed api routes file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,9 +1,11 @@
-//create const variables for data source arrays
-const router = require('express').Router();
-const store = require("../db/store")
+//create variables for the router functions in express and calls the uniqueid file which helps with creating a unique id on each note
 
+const router = require('express').Router();
+const remember= require("../db/uniqueid")
+
+//router.get will find the notes and render in the response.  If the response is no good, it will return a 500 (no server) error
 router.get('/notes', (req, res) => {
-    store
+    remember
         .getNotes()
         .then((notes) => {
         return res.json(notes);
@@ -11,17 +13,27 @@ router.get('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
+//router.post will write the notes and display on the user screen
+
 router.post('/notes', (req, res) => {
 
-    store
+    remember
         .addNote(req.body)
         .then((note) => res.json(note))
         .catch((err) => res.status(500).json(err));
 });
 
-///ROUTING
+
+//now that we have a unique id, this is the function to delete a note.  id is defined in the uniqueid.js file.  Router.delete handles this in it's methods.
+
+router.delete('/notes/:id', (req, res) => {
+remember
+.removeNote(req.params.id)
+.then(() => res.json( {ok:true}))
+.catch((err) => res.status(500).json(err));
 
 
+});
 
 
 module.exports = router;
