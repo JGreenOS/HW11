498dbbf26629b2a8f0c250cbaf1dcb1f226d2443
JGreenOS <jsciteach@gmail.com>
2021-03-21 14:29:50 -0400

    final fix updating formatting

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,16 +1,16 @@
 //create variables for the router functions in express and calls the uniqueid file which helps with creating a unique id on each note
 
 const router = require('express').Router();
-const remember= require("../db/uniqueid")
+const remember = require("../db/uniqueid")
 
 //router.get will find the notes and render in the response.  If the response is no good, it will return a 500 (no server) error
 router.get('/notes', (req, res) => {
     remember
         .getNotes()
         .then((notes) => {
-        return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err));
 });
 
 //router.post will write the notes and display on the user screen
@@ -27,10 +27,10 @@ router.post('/notes', (req, res) => {
 //now that we have a unique id, this is the function to delete a note.  id is defined in the uniqueid.js file.  Router.delete handles this in it's methods.
 
 router.delete('/notes/:id', (req, res) => {
-remember
-.removeNote(req.params.id)
-.then(() => res.json( {ok:true}))
-.catch((err) => res.status(500).json(err));
+    remember
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
 
 
 });
--- Develop/server.js
+++ Develop/server.js
@@ -6,14 +6,15 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({  extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 //router
 
- 
+
 
 app.listen(PORT, () => {
-console.log(`App listening on PORT: ${PORT}`)});
+    console.log(`App listening on PORT: ${PORT}`)
+});
